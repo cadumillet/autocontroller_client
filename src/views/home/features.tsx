ea8b43baace5b819383249b8963cfc7cd288e409
Carlos Millet <cadumillet@icloud.com>
2023-12-22 11:19:26 -0300

    add content

--- src/views/home/features.tsx
+++ src/views/home/features.tsx
@@ -2,23 +2,23 @@ import { InboxIcon, TrashIcon, UsersIcon } from "@heroicons/react/24/outline";
 
 const features = [
   {
-    name: "Unlimited inboxes",
+    name: "Controle",
     description:
-      "Non quo aperiam repellendus quas est est. Eos aut dolore aut ut sit nesciunt. Ex tempora quia. Sit nobis consequatur dolores incidunt.",
+      "Com soluções avançadas, oferecemos controle efetivo da sua frota, proporcionando suporte à logística e gerenciamento de riscos.",
     href: "#",
     icon: InboxIcon,
   },
   {
-    name: "Manage team members",
+    name: "Tecnologia",
     description:
-      "Vero eum voluptatem aliquid nostrum voluptatem. Vitae esse natus. Earum nihil deserunt eos quasi cupiditate. A inventore et molestiae natus.",
+      "Utilizando as melhores tecnologias do mercado, asseguramos tranquilidade e segurança no monitoramento de sua frota, com comunicação de dados confiáveis de localização.",
     href: "#",
     icon: UsersIcon,
   },
   {
-    name: "Spam report",
+    name: "Segurança",
     description:
-      "Et quod quaerat dolorem quaerat architecto aliquam accusantium. Ex adipisci et doloremque autem quia quam. Quis eos molestiae at iure impedit.",
+      "Oferecemos produtos e serviços que proporcionam a segurança de seus colaboradores e patrimônio.",
     href: "#",
     icon: TrashIcon,
   },
@@ -30,11 +30,11 @@ export function Features() {
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0">
           <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-            Stay on top of customer support
+            Sua frota em boas mãos
           </h2>
           <p className="mt-6 text-lg leading-8 text-gray-300">
-            Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam
-            voluptatum cupiditate veritatis in accusamus quisquam.
+            Gerencie suas operações com toda segurança e tranquilidade
+            utilizando um rastreamento inteligente.
           </p>
         </div>
         <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
@@ -52,14 +52,6 @@ export function Features() {
                 </dt>
                 <dd className="mt-1 flex flex-auto flex-col text-base leading-7 text-gray-300">
                   <p className="flex-auto">{feature.description}</p>
-                  <p className="mt-6">
-                    <a
-                      href={feature.href}
-                      className="text-sm font-semibold leading-6 text-indigo-400"
-                    >
-                      Learn more <span aria-hidden="true">→</span>
-                    </a>
-                  </p>
                 </dd>
               </div>
             ))}
